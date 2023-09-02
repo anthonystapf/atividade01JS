485c95e1a9390833ae3b8b271253f6d821560fdb
Anthony Stapf <125614058+anthonystapf@users.noreply.github.com>
2023-09-01 22:27:05 -0300

    Update atv02.js

--- atv02.js
+++ atv02.js
@@ -42,7 +42,7 @@ chamada "changeMe" usando a palavra-chave "let" e atribua a ela o valor
 booleano "true". Os booleanos são um tipo de dados em JavaScript que
 representam verdadeiro (true) ou falso (false).*/
 
-
+changeMe = false;
 
 /*7)Em seguida, na linha após a declaração de "changeMe", reatribua o valor de
 "changeMe" para o booleano "false". A palavra-chave "let" permite que você
